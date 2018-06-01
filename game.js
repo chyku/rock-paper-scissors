cd9c9dc313027b56c9d3e44da058d3d6e37c1fbb
chyku <ky.cu303@gmail.com>
2018-06-01 05:00:39 -0400

    finished functionality

--- game.js
+++ game.js
@@ -1,12 +1,19 @@
 function game(hand){
   var opp = Math.floor(Math.random() * Math.floor(3));
-  var results = ["Tie", "Win1", "Win2"];
-  alert(results[Math.abs(opp - hand)]);
+  var results = ["Tie", "Win", "Lose"];
+  var opposition = ["Rock", "Paper", "Scissors"];
+  var result = hand - opp;
+  if (result < 0){
+  	result += 3;
+  }
+  document.getElementById("results").innerHTML = opposition[opp] + " " + results[result];
 }
 
-/* Opp - hand results
+/* hand - opp results
 0 - tie
 1 - 2-1 or 1-0 = ez win on one side
+	pos = hand win
 2 - 2-0
+	pos = opp win
 rock = 0, paper = 1, scissors = 2
 */
